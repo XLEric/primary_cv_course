4548752e37452f0763c135feb0401ddeac92d7a1
x.l.eric <koke8756@qq.com>
2020-08-26 16:19:08 +0800

    add ch05

---
+++ chapter_05/data_iter/utils_mt.py
@@ -0,0 +1,87 @@
+#coding:utf-8
+import numpy as np
+import cv2
+
+import os
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import random
+
+
+def set_seed(seed = 666):
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        cudnn.deterministic = True
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * max(img.shape[0:2])) + 1  # line thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]# color
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl)
+    if label:
+        tf = max(tl - 2, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0] # label size
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3 # 字体的bbox
+        cv2.rectangle(img, c1, c2, color, -1)  # filled rectangle
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 4, [225, 255, 255],\
+        thickness=tf, lineType=cv2.LINE_AA)
+
+def IoU(box, boxes):
+    """Compute IoU between detect box and gt boxes
+
+    Parameters:
+    ----------
+    box: numpy array , shape (5, ): x1, y1, x2, y2, score
+        input box
+    boxes: numpy array, shape (n, 4): x1, y1, x2, y2
+        input ground truth boxes
+
+    Returns:
+    -------
+    ovr: numpy.array, shape (n, )
+        IoU
+    """
+    box_area = (box[2] - box[0] + 1) * (box[3] - box[1] + 1)
+    area = (boxes[:, 2] - boxes[:, 0] + 1) * (boxes[:, 3] - boxes[:, 1] + 1)
+    xx1 = np.maximum(box[0], boxes[:, 0])
+    yy1 = np.maximum(box[1], boxes[:, 1])
+    xx2 = np.minimum(box[2], boxes[:, 2])
+    yy2 = np.minimum(box[3], boxes[:, 3])
+
+    # compute the width and height of the bounding box
+    w = np.maximum(0, xx2 - xx1 + 1)
+    h = np.maximum(0, yy2 - yy1 + 1)
+
+    inter = w * h
+    ovr = inter / (box_area + area - inter)
+    return ovr
+
+
+def convert_to_square(bbox):
+    """Convert bbox to square
+
+    Parameters:
+    ----------
+    bbox: numpy array , shape n x 5
+        input bbox
+
+    Returns:
+    -------
+    square bbox
+    """
+    square_bbox = bbox.copy()
+
+    h = bbox[:, 3] - bbox[:, 1] + 1
+    w = bbox[:, 2] - bbox[:, 0] + 1
+    max_side = np.maximum(h,w)
+    square_bbox[:, 0] = bbox[:, 0] + w*0.5 - max_side*0.5
+    square_bbox[:, 1] = bbox[:, 1] + h*0.5 - max_side*0.5
+    square_bbox[:, 2] = square_bbox[:, 0] + max_side - 1
+    square_bbox[:, 3] = square_bbox[:, 1] + max_side - 1
+    return square_bbox
---
+++ chapter_05/train_xnet.py
@@ -0,0 +1,170 @@
+#-*-coding:utf-8-*-
+# date:2019-05-20
+# Author: X.L.Eric
+# function: train P-Net/R-Net/O-Net
+from data_iter.mt_data_xnet_iter import *
+from data_iter.utils_mt import *
+import os
+import numpy as np
+from core.models import PNet,RNet,ONet,LossFn
+import time
+import datetime
+import torch.nn as nn
+from torch.autograd import Variable
+import argparse
+
+def mkdir_(path, flag_rm=False):
+    if os.path.exists(path):
+        if flag_rm == True:
+            shutil.rmtree(path)
+            os.mkdir(path)
+            print('remove {} done ~ '.format(path))
+    else:
+        os.mkdir(path)
+
+def trainer(ops):
+
+    set_seed(ops.seed)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device('cuda:0' if use_cuda else 'cpu')
+    # datasets
+    dataset = LoadImagesAndLabels(pattern = ops.pattern,path_img = ops.path_img,path_anno = ops.path_anno,batch_size = ops.batch_size)
+    print('dataset len : ',dataset.__len__())
+    dataloader = DataLoader(dataset,
+                            batch_size=1,
+                            num_workers=ops.num_workers,
+                            shuffle=True,
+                            pin_memory=False,
+                            drop_last = True)
+
+    if ops.pattern == 'P-Net':
+        m_XNet = PNet()
+    elif ops.pattern == 'R-Net':
+        m_XNet = RNet()
+    elif ops.pattern == 'O-Net':
+        m_XNet = ONet()
+
+    print('{} : \n'.format(ops.pattern),m_XNet)
+    m_XNet = m_XNet.to(device)
+
+    m_loss = LossFn()
+
+    if ops.Optimizer_X == 'Adam':
+        optimizer = torch.optim.Adam(m_XNet.parameters(), lr=ops.init_lr, betas=(0.9, 0.99),weight_decay=1e-6)
+    elif ops.Optimizer_X == 'SGD':
+        optimizer = torch.optim.SGD(m_XNet.parameters(), lr = ops.init_lr, momentum=0.9,weight_decay=1e-6)
+    elif ops.Optimizer_X == 'RMSprop':
+        optimizer=torch.optim.RMSprop(m_XNet.parameters(),lr=ops.init_lr,alpha=0.9,weight_decay=1e-6)
+    else:
+        print('------>>> Optimizer init error : ',ops.Optimizer_X)
+
+    # load finetune model
+    if os.access(ops.ft_model,os.F_OK):
+        chkpt = torch.load(ops.ft_model, map_location=device)
+        print('chkpt:\n',ops.ft_model)
+        m_XNet.load_state_dict(chkpt)
+
+    # train
+    print('  epoch : ',ops.epochs)
+    best_loss = np.inf
+    loss_mean = 0.
+    loss_cls_mean = 0.
+    loss_idx = 0.
+    init_lr = ops.init_lr
+
+    for epoch in range(0, ops.epochs):
+
+        if loss_idx!=0:
+            if best_loss > (loss_mean/loss_idx):
+                best_loss = loss_mean/loss_idx
+            else:
+                init_lr = init_lr*0.1
+                set_learning_rate(optimizer, init_lr)
+
+        loss_mean = 0.
+        loss_cls_mean = 0.
+        loss_idx = 0.
+
+        print('\nepoch %d '%epoch)
+        m_XNet = m_XNet.train()
+        random.shuffle (dataset.annotations)# shuffle 图片组合
+
+        for i, (imgs,gt_labels,gt_offsets,pos_num,part_num,neg_num) in enumerate(dataloader):
+            imgs = imgs.squeeze(0)
+            gt_labels = gt_labels.squeeze(0)
+            gt_offsets = gt_offsets.squeeze(0)
+            # print('imgs size {}, labels size {}, offsets size {}'.format(imgs.size(),gt_labels.size(),gt_offsets.size()))
+
+            if use_cuda:
+                imgs = imgs.cuda()  # (bs, 3, h, w)
+                gt_labels = gt_labels.cuda()
+                gt_offsets = gt_offsets.cuda()
+
+            cls_pred, box_offset_pred = m_XNet(imgs)
+
+            cls_loss = m_loss.focal_Loss(gt_labels,cls_pred)
+            box_offset_loss = m_loss.box_loss(gt_labels,gt_offsets,box_offset_pred)
+
+            if ops.pattern == 'O-Net':
+                all_loss = cls_loss*1.0+box_offset_loss*0.4
+            elif ops.pattern == 'R-Net':
+                all_loss = cls_loss*1.0+box_offset_loss*0.6
+            else:
+                all_loss = cls_loss*1.0+box_offset_loss*1.0
+
+            loss_mean += all_loss.item()
+            loss_cls_mean += cls_loss.item()
+            loss_idx += 1.
+
+            optimizer.zero_grad()
+            all_loss.backward()
+            optimizer.step()
+
+            if i%5 == 0:
+                loc_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+                print("[%s]-<%s>Epoch: %d, [%d/%d],lr:%.6f,Loss:%.5f - Mean Loss:%.5f - Mean cls loss:%5f,cls_loss:%.5f ,bbox_loss:%.5f,imgs_batch: %4d,best_loss: %.5f" \
+                % (loc_time,ops.pattern,epoch, i,dataset.__len__(), optimizer.param_groups[0]['lr'], \
+                all_loss.item(),loss_mean/loss_idx,loss_cls_mean/loss_idx,cls_loss.item(),box_offset_loss.item(),imgs.size()[0],best_loss), ' ->pos:{},part:{},neg:{}'.format(pos_num.item(),part_num.item(),neg_num.item()))
+
+            if i%50==0 and i>1:
+                accuracy=compute_accuracy(cls_pred,gt_labels)
+                print("\n  ------------- >>>  accuracy: %f\n"%(accuracy.item()))
+                accuracy=compute_accuracy(cls_pred,gt_labels)
+                torch.save(m_XNet.state_dict(), ops.ckpt + '{}_latest.pth'.format(ops.pattern))
+            if i%80==0 and i>1:
+                torch.save(m_XNet.state_dict(), ops.ckpt + '{}_epoch-{}.pth'.format(ops.pattern,epoch))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=' Project X-Net Train')
+    parser.add_argument('--seed', type=int, default = 66,
+        help = 'seed') # 设置随机种子
+    parser.add_argument('--path_anno', type=str, default = "./datasets/wider_face_train.txt",
+        help = 'path_anno')
+    parser.add_argument('--path_img', type=str, default = "./datasets/WIDER_train/images/",
+        help = 'path_img')
+    parser.add_argument('--batch_size', type=int, default = 2048,
+        help = 'batch_size')
+    parser.add_argument('--init_lr', type=float, default = 1e-3,
+        help = 'init_lr')
+    parser.add_argument('--num_workers', type=int, default = 16,
+        help = 'num_workers')
+    parser.add_argument('--epochs', type=int, default = 1000,
+        help = 'epochs')
+    parser.add_argument('--ft_model', type=str, default = './ckpt/R-Net_latest.pth',#./ckpt/R-Net_latest.pth
+        help = 'ft_model')
+    parser.add_argument('--ckpt', type=str, default = './ckpt/',
+        help = 'ckpt')
+    parser.add_argument('--Optimizer_X', type=str, default = 'Adam',
+        help = 'Optimizer_X：Adam,SGD,RMSprop')
+    parser.add_argument('--pattern', type=str, default = 'R-Net',
+        help = 'pattern：P-Net,R-Net,O-Net')
+
+    #--------------------------------------------------------------------------
+    args = parser.parse_args()# 解析添加参数
+
+    mkdir_(args.ckpt)
+
+    trainer(ops = args)
+    print('well done : {}'.format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
